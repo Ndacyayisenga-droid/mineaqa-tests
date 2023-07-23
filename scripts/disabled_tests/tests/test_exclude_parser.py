d50a580c53ea8ec6db8918494224e445770c9c63
Ndacyayisenga-droid <ndacyayinoah@gmail.com>
2023-07-24 01:02:05 +0300

    fix code

--- scripts/disabled_tests/tests/test_exclude_parser.py
+++ scripts/disabled_tests/tests/test_exclude_parser.py
@@ -1,6 +1,5 @@
 import json
 import os
-import re
 from unittest import TestCase
 
 from exclude_parser import transform_platform, parse_all_files
@@ -46,15 +45,3 @@ class Test(TestCase):
         actual = [excl.to_scheme() for excl in parse_all_files([input_file1, input_file2])]
         self.assertEqual(expected, actual)
 
-
-def transform_platform(platform):
-    # Updated regex pattern to handle '-' and '_', and added a lowercasing step.
-    split_pattern = re.compile(r'[-_]')
-
-    # Split the platform string using the regex pattern.
-    parts = split_pattern.split(platform)
-
-    # Lowercase each part before mapping it to the platform_map.
-    parts = [part.lower() for part in parts]
-
-    return "_".join([platform_map.get(part, part) for part in parts if part])
